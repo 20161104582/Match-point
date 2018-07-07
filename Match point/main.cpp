4201dd8d72f09679bf6122fe5dcedf32e199501c
20161104582 <a20161104582@scy.local>
2018-07-07 18:36:42 +0800

    Initial Commit

---
+++ Match point/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Match point
+//
+//  Created by a20161104582 on 2018/7/7.
+//  Copyright © 2018年 a20161104582. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
